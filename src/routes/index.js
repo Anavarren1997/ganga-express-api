712181bb377374b182e67edc1254159fbae92767
Anavarren1997 <anavarren@alumnos.unex.es>
2024-03-17 19:59:28 +0100

    refactorized router biding

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,14 @@
 import gangaRoutes from './ganga.routes.js';
 import palletRoutes from './pallet.routes.js';
-import { bindAsyncRoutes } from '../utils/asyncRouteBinder';
+import { bindModelRoutes } from '../utils/asyncRouteBinder';
 import express from 'express';
 
 const router = express.Router();
 
-// Crear un nuevo router para las rutas de pallets
-const palletsRouter = express.Router();
-bindAsyncRoutes(palletsRouter, palletRoutes);
-router.use('/pallets', palletsRouter);
+// Bindear rutas de pallets
+bindModelRoutes(router, '/pallets', palletRoutes);
 
-// Crear un nuevo router para las rutas de gangas
-const gangasRouter = express.Router();
-bindAsyncRoutes(gangasRouter, gangaRoutes);
-router.use('/gangas', gangasRouter);
+// Bindear rutas de gangas
+bindModelRoutes(router, '/gangas', gangaRoutes);
 
 export default router;
--- src/utils/asyncRouteBinder.ts
+++ src/utils/asyncRouteBinder.ts
@@ -13,9 +13,25 @@ type RouteDefinition = {
  * @param router
  * @param routes
  */
-export function bindAsyncRoutes(router: Router, routes: RouteDefinition[]) {
+function bindAsyncRoutes(router: Router, routes: RouteDefinition[]) {
   routes.forEach(({ method, path, controller, middlewares = [] }) => {
     // No es necesario mapear middlewares a sí mismos, puedes pasarlos directamente
     router[method](path, ...middlewares, asyncHandler(controller));
   });
 }
+
+/**
+ * Bindea un array de rutas convirtiéndolo en un Router de Express.
+ * @param router El router principal al cual se añadirán las rutas.
+ * @param pathPrefix El prefijo de ruta para las rutas del modelo.
+ * @param modelRoutes Las rutas a ser añadidas bajo el prefijo dado.
+ */
+export const bindModelRoutes = (
+  router: Router,
+  pathPrefix: string,
+  modelRoutes: RouteDefinition[]
+): void => {
+  const modelRouter = Router();
+  bindAsyncRoutes(modelRouter, modelRoutes);
+  router.use(pathPrefix, modelRouter);
+};
